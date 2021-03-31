fa3a975d5549399e6f739667feb69f306d5b2054
mvirando <mvirando@cisco.com>
2021-03-31 09:20:41 +0100

    Made changes to see if import works

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,26 @@ import postcss from 'rollup-plugin-postcss';
 import {uglify} from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
+const makeExternalPredicate = (externalArr) => {
+  if (externalArr.length === 0) {
+    return () => false;
+  }
+  const pattern = new RegExp(`^(${externalArr.join('|')})($|/)`);
+  return (id) => pattern.test(id);
+};
+
 export default {
-  input: './src/index.ts',
+  input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
       format: 'cjs',
     },
   ],
-  external: [
+  external: makeExternalPredicate([
     ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependencies || {}),
-  ],
+  ]),
   plugins: [
     typescript({
       typescript: require('typescript'),
